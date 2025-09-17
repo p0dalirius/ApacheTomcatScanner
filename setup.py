3ad205c6cde61d9c0aa9395ce4cb8ace636c1b85
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-09-17 09:33:25 +0200

    Release 3.8.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name="apachetomcatscanner",
-    version="3.7.1",
+    version="3.8.0",
     description="",
     url="https://github.com/p0dalirius/ApacheTomcatScanner",
     author="Podalirius",
